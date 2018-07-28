cc65fa5fbab2bf627734e8d1418de7034875566b
gfreezy <gfreezy@gmail.com>
2018-07-29 00:29:40 +0800

    bump dependencies

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='slacker-asyncio',
-    version='0.9.60',
+    version='0.9.61',
     packages=['slacker'],
     description='Slack API asyncio client based on os\'s slacker',
     author='gfreezy',
     author_email='gfreezy@gmail.com',
     url='http://github.com/gfreezy/slacker-asyncio/',
-    install_requires=['aiohttp ~= 3.0.5'],
+    install_requires=['aiohttp ~= 3.3'],
     license='http://www.apache.org/licenses/LICENSE-2.0',
     test_suite='tests',
     classifiers=[
@@ -17,7 +17,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='slack api asyncio'
 )
